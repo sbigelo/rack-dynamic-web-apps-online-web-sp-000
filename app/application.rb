1b08143eee96158d073bc2eaeec6862e5b7502fe
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-04 21:01:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,23 +1,10 @@
 class Application
 
+  def call(env)
+    return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
+  end
 
-   def call(env)
-     resp = Rack::Response.new
-
-     num_1 = Kernel.rand(1..20)
-     num_2 = Kernel.rand(1..20)
-     num_3 = Kernel.rand(1..20)
-
-     resp.write "#{num_1}\n"
-     resp.write "#{num_2}\n"
-     resp.write "#{num_3}\n"
-
-     if num_1==num_2 && num_2==num_3
-       resp.write "You Win"
-     else
-       resp.write "You Lose"
-     end
-
-     resp.finish
-   end
+  def pretty_response
+    (Time.now.to_i < 12:00) ?  ["<em>Good Morning!</em>"] : ["<strong>Good Afternoon!</strong>"]
+  end
 end
